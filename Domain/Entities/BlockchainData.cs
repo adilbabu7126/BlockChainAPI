3190d445243e20c969be17213692b4f3fb27f322
Adil Babu Thekkarakath <adilbabudrizzle@gmail.com>
2025-09-29 16:42:37 +0400

    Added EntityFramework

---
+++ Domain/Entities/BlockchainData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain.Domain.Entities
+{
+    public class BlockchainData
+    {
+        public int Id { get; set; }
+        //[Required]
+        public string BlockchainType { get; set; } = string.Empty; // BTC, ETH, DASH, LTC
+        public string? Hash { get; set; }
+        public long? Height { get; set; }
+        public string RawJson { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BlockChain.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlockChain.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<BlockchainData> BlockchainData { get; set; } = null!;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BlockchainData>(b =>
+            {
+                //b.ToTable("BlockchainData");
+                b.HasKey(x => x.Id);
+                b.Property(x => x.BlockchainType).IsRequired();
+                b.Property(x => x.RawJson).IsRequired();
+                b.Property(x => x.CreatedAt).IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
